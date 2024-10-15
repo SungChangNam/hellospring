bcfec6c8a3e62f06c218179a8bb6401495b82acb
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2024-10-15 14:36:16 +0900

    순수 JDBC로 구현하기

--- src/main/java/hello/hellspring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellspring/repository/MemoryMemberRepository.java
@@ -1,7 +1,7 @@
 package hello.hellspring.repository;
 
 import hello.hellspring.domain.Member;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.*;
 
@@ -9,31 +9,26 @@ import java.util.*;
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
-
-
+    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
     }
-
     @Override
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
-
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
-
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
-
     public void clearStore() {
         store.clear();
     }
